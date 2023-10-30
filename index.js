68c674f825ab1918028570657fde458b2d3846fc
RANJAN <141898565+mranja@users.noreply.github.com>
2023-10-30 19:28:30 +0530

    index.js

--- index.js
+++ index.js
@@ -1,84 +1,78 @@
 var boxes = document.querySelectorAll(".box");
-console.log(boxes)
+console.log(boxes);
 
-
-
-var winningCominaations =[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+var winnigCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ];
 
-var Xattempts = [];
-var Oattempts = [];
-
-ans=[true,true,true]
+var xAttempts = [];
+var oAttempts = [];
 
 var result = document.getElementById("result");
 var message = document.getElementById("message");
 var button = document.getElementById("button");
 
-boxes.forEach(box => {
-    box.onclick = handeleClick;
+boxes.forEach(box =>{
+    box.onclick = handleClick;
+})
 
-    
-});
+var click = 0;
 
-var click = 0
-
-function handeleClick(e){
-    console.log("e",e)
+function handleClick(e){
+    console.log("e", e)
     var id_d = e.target.id;
 
     var texttobeinserted = document.createElement("p");
     boxes[id_d-1].append(texttobeinserted);
 
     if(click%2==0){
-        Xattempts.push(id_d-1)
-        texttobeinserted.innerHTML="X";
-        texttobeinserted.style.color= "red"
-        checkResult(winningCominaations,Xattempts,"X")
+        xAttempts.push(id_d-1);
+        texttobeinserted.innerHTML = "X";
+        texttobeinserted.style.color = "red";
+        checkResult(winnigCombinations, xAttempts, "X");
     }else{
-        Xattempts.push(id_d-1)
-        texttobeinserted.innerHTML="O";
-        texttobeinserted.style.color= "red"
-        checkResult(winningCominaations,Oattempts,"O")
+        oAttempts.push(id_d-1);
+        texttobeinserted.innerHTML = "O";
+        texttobeinserted.style.color = "red";
+        checkResult(winnigCombinations, oAttempts, "O");
     }
-    if(click==9){
-        result.style.visibility="visible"
-        message.innerHTML="Its's a tie!"
+    click++;
+    if(click == 9){
+        result.style.visibility = "visible";
+        message.innerHTML = "It's a tie!";
     }
-    click++
 }
 
-function checkResult(winningCominaations,attempts,players){
-    var ans= [];
-    var count=0;
-    for(var i=0; i<winningCominaations.length;i++){
-        if(Array.isArray(winningCominaations[i])){
-            checkResult(winningCominaations[i],attempts,players);
+function checkResult(winnigCombinations, attempts, player){
 
+    var ans = [];
+    var count = 0;
+    for(var i=0; i<winnigCombinations.length; i++){
+        if(Array.isArray(winnigCombinations[i])){
+            checkResult(winnigCombinations[i], attempts, player);
         }else{
-            if(attempts.includes(winningCominaations[i])){
+            if(attempts.includes(winnigCombinations[i])){
                 ans.push(true);
                 count++;
-
             }else{
                 ans.push(false);
             }
         }
     }
-    if(ans.every(answer => answer == true)&&count>2){
-        result.style.visibility="visible";
-        message.innerHTML =`${players} won the game!`;
-    }
+
+    if(ans.every((answer) => answer == true) && count>2){
+        result.style.visibility = "visible";
+        message.innerHTML = `${player} won the game!`;
+    }    
 }
 
-button.onclick= () =>{
+button.onclick = () =>{
     window.location.reload();
 }
